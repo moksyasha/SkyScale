daab7557857f6b3397bd0ec4550a7ef43c44ba0b
moksyasha <korneich.nik@gmail.com>
2024-05-08 23:26:30 +0700

    comments, deleted some trash

--- MoksVSR/models/moksvsr/MoksVSR.py
+++ MoksVSR/models/moksvsr/MoksVSR.py
@@ -5,7 +5,7 @@ from models.flow.FastFlowNet_v2 import FastFlowNet
 import time
 from torchvision.ops import deform_conv2d
 import math
-
+import matplotlib.pyplot as plt
 
 class ResBlock(nn.Module):
     def __init__(self, nc):
@@ -54,6 +54,7 @@ class ResBlocksWithInputConv(nn.Module):
         return self.result(feat)
 
 
+# basic vsr with new optical flow model
 class MoksVSR(nn.Module):
     def __init__(self, nb=30, nc=64):
         super().__init__()
@@ -181,7 +182,7 @@ class DeformableAlignment(nn.Module):
         self.dilation = 1
         self.max_residue_magnitude = max_residue_magnitude
         self.kernel = kernel
-        # convolution for current feat and reference (feat_propagated, prev_feauter, flow1, flow2)
+        # convolution for current feat and reference (feat_propagated, prev_featur, flow1, flow2)
         # to get offsets and masks
         self.conv_offset = nn.Sequential(
             nn.Conv2d(self.out_channels * 3 + 4, self.out_channels, 3, 1, 1),
@@ -236,6 +237,7 @@ class DeformableAlignment(nn.Module):
                                        self.dilation, mask)
 
 
+#own model
 class MoksPlus(nn.Module):
     def __init__(self, nb=10, nf=64):
         super().__init__()
@@ -265,6 +267,7 @@ class MoksPlus(nn.Module):
         self.reconstruction = ResBlocksWithInputConv(
             3 * self.num_features, self.num_features, 5)
 
+        # before pixel_shuffle
         self.psh_upsample = nn.Conv2d(
             self.num_features,
             self.num_features * 2 * 2,
@@ -281,12 +284,11 @@ class MoksPlus(nn.Module):
 
     
     def upsample(self, lr, feats):
-        """Compute the output image given the features.
+        """compute the output image given the features.
 
         Args:
-            lqs (tensor): Input low quality (LQ) sequence with
-                shape (n, t, c, h, w).
-            feats (dict): The features from the propagation branches.
+            lqs : low quality (LQ) sequence with shape (n, t, c, h, w).
+            feats (dict): features from the propagation branches.
 
         Returns:
             Tensor: Output HR sequence with shape (n, t, c, 4h, 4w).
@@ -304,7 +306,7 @@ class MoksPlus(nn.Module):
             hr.insert(0, feats['spatial'][mapping_idx[i]])
             hr = torch.cat(hr, dim=1)
             #hr = [feature, back, forward] / 192
-            # pass to rhe residual blocks
+            # pass to the residual blocks
             hr = self.reconstruction(hr)
             hr = self.lrelu(F.pixel_shuffle(self.psh_upsample(hr), 2))
             hr = self.lrelu(F.pixel_shuffle(self.psh_upsample(hr), 2))
@@ -315,11 +317,11 @@ class MoksPlus(nn.Module):
             
             outputs.append(hr)
         res = torch.stack(outputs, dim=1)
-        #print(res)
+
         return res
 
     def propagate(self, feats, flows, module_name):
-        """Propagate the latent features throughout the sequence.
+        """Propagate the  features through the sequence.
         """
         n, t, _, h, w = flows.size()
 
@@ -340,6 +342,7 @@ class MoksPlus(nn.Module):
         frame = 2
         for i, idx in enumerate(frame_idx):
             feat_current = feats["spatial"][idx]
+
             frame -= 1
             # first frame has no flow
             if i > 0:
@@ -360,7 +363,7 @@ class MoksPlus(nn.Module):
                 # align current feature with reference feat_prop, feat_prev
                 feat_deform = self.deform_align[module_name](feat_current, feat_deform,
                                     feat_prev, flow_prev, flow_current)
-            
+
             # 1 128 h w
             # concat aligned by flow and by deform conv
             feat = torch.cat([feat_current] + [feat_deform], dim=1)
@@ -382,7 +385,7 @@ class MoksPlus(nn.Module):
     def forward(self, lr):
         """Forward function.
         Args:
-            lrs (Tensor): Input LR sequence with shape (n, t, c, h, w)
+            lrs (Tensor): input LR sequence with shape (n, t, c, h, w)
             batch of clips, imgs per clip, channels, h, w
         Returns:
             Tensor: Output HR sequence with shape (n, t, c, 4h, 4w)
@@ -395,9 +398,6 @@ class MoksPlus(nn.Module):
 
         forward_flows, backward_flows = self.compute_flows_fast(lr)
 
-        # backward propagation
-        # 1 64 h w
-        #features = lr.new_zeros((N, self.mid_channel, H, W)) # initialize features as zeros with same dtype and device as x
         # 4 64 h w
         feats = {}
         feats_all = self.feat_extract(lr[0])
@@ -470,120 +470,3 @@ class MoksPlus(nn.Module):
         forward_flows = self.fastflow(torch.cat((x_start, x_end), 1))#.view(N, T-1, 2, H, W)
         backward_flows = self.fastflow(torch.cat((x_end, x_start), 1))#.view(N, T-1, 2, H, W)
         return forward_flows.unsqueeze(0), backward_flows.unsqueeze(0)
-
-        
-
-class TestMoksVSR(nn.Module):
-    def __init__(self, nb=10, nc=16):
-        super().__init__()
-        self.num_blocks = nb
-        self.mid_channel = nc
-        self.fastflow = FastFlowNet().cuda().eval()
-        self.fastflow.load_state_dict(torch.load('/home/moksyasha/Projects/SkyScale/MoksVSR/checkpoints/fastflownet_ft_mix.pth'))
-        self.lrelu = nn.LeakyReLU(0.1)
-        self.forward_res = nn.Sequential(nn.Conv2d(self.mid_channel+3, self.mid_channel, 3, stride=1, padding=1), self.lrelu, *[ResBlock(self.mid_channel) for _ in range(self.num_blocks)])
-        self.backward_res = nn.Sequential(nn.Conv2d(self.mid_channel+3, self.mid_channel, 3, stride=1, padding=1), self.lrelu, *[ResBlock(self.mid_channel) for _ in range(self.num_blocks)])
-     ##
-        self.fusion = nn.Conv2d(2*self.mid_channel, self.mid_channel, 3, stride=1, padding=1)
-        self.up_conv1 = nn.Conv2d(self.mid_channel, 4*self.mid_channel, 3, stride=1, padding=1)
-        self.up_conv2 = nn.Conv2d(self.mid_channel, 4*self.mid_channel, 3, stride=1, padding=1)
-        self.hr_conv = nn.Conv2d(self.mid_channel, 3, 3, stride=1, padding=1)
-
-    def flow_warp(self, x,
-              flow,
-              interpolation='bilinear',
-              padding_mode='zeros',
-              align_corners=True):
-        '''Warp an image or a feature map with optical flow.
-        Args:
-            x (Tensor): Tensor with size (n, c, h, w).
-            flow (Tensor): Tensor with size (n, h, w, 2). The last dimension is
-                a two-channel, denoting the width and height relative offsets.
-                Note that the values are not normalized to [-1, 1].
-            interpolation (str): Interpolation mode: 'nearest' or 'bilinear'.
-                Default: 'bilinear'.
-            padding_mode (str): Padding mode: 'zeros' or 'border' or 'reflection'.
-                Default: 'zeros'.
-            align_corners (bool): Whether align corners. Default: True.
-        Returns:
-            Tensor: Warped image or feature map.
-        '''
-        _N, _C, H, W = x.shape
-        device = flow.device
-        grid_y, grid_x = torch.meshgrid(
-            torch.arange(0, H, device=device, dtype=x.dtype),
-            torch.arange(0, W, device=device, dtype=x.dtype))
-        grid = torch.stack((grid_x, grid_y), dim=2)
-        grid_flow = flow.permute((0, 2, 3, 1)) + grid # flow shape: NCHW to NHWC
-        
-        # normalize grid flows to [-1, 1]
-        grid_flow_x = 2.0 * grid_flow[:, :, :, 0] / max(W - 1, 1) - 1.0
-        grid_flow_y = 2.0 * grid_flow[:, :, :, 1] / max(H - 1, 1) - 1.0
-        grid_flow = torch.stack((grid_flow_x, grid_flow_y), dim=3)
-        
-        output = F.grid_sample(
-            x,
-            grid_flow,
-            mode=interpolation,
-            padding_mode=padding_mode,
-            align_corners=align_corners)
-        return output
-
-    def compute_flows_fast(self, lrs):
-        # compute optical flow by FastFlowNet
-        N, T, C, H, W = lrs.shape
-        # 19 6 180 320
-        x_start = lrs[:, :-1, :, :, :].reshape(-1, C, H, W) # reshape from (N, T, C, H, W) into (N*T*C, H, W)
-        x_end = lrs[:, 1:, :, :, :].reshape(-1, C, H, W)
-        forward_flows = self.fastflow(torch.cat((x_start, x_end), 1))#.view(N, T-1, 2, H, W)
-        backward_flows = self.fastflow(torch.cat((x_end, x_start), 1))#.view(N, T-1, 2, H, W)
-        return forward_flows.unsqueeze(0), backward_flows.unsqueeze(0)
-
-    def forward(self, lr):
-        """Forward function.
-        Args:
-            lrs (Tensor): Input LR sequence with shape (n, t, c, h, w)
-            batch of clips, imgs per clip, channels, h, w
-        Returns:
-            Tensor: Output HR sequence with shape (n, t, c, 4h, 4w)
-        """
-        #1 20 3 180 320
-        N, T, C, H, W = lr.shape
-        # print(N, T, C, H, W)
-        # flows shape
-        # 19 2 180 320
-
-        forward_flows, backward_flows = self.compute_flows_fast(lr)
-
-        # backward propagation
-        # 1 64 h w
-        features = lr.new_zeros((N, self.mid_channel, H, W)) # initialize features as zeros with same dtype and device as x
-        outputs = []
-        for t in range(T-1, -1, -1):
-            if t != T-1: # warp features if not looking at last frame of clip 
-                features = self.flow_warp(features, backward_flows[:, t, :, :, :]) # feature warping (W step)
-            features = torch.cat((features, lr[:, t, :, :, :]), dim=1) # concatenate NCHW tensors along C axis
-            features = self.backward_res(features) # residual processing (R step)
-            outputs.append(features)
-        outputs = outputs[::-1]
-        
-        # forward propagation
-        features = lr.new_zeros((N, self.mid_channel, H, W)) # initialize features as zeros with same dtype and device as x
-        for t in range(T):
-            lr_curr = lr[:, t, :, :, :]
-            if t != 0: # warp features if not looking at first frame of clip 
-                features = self.flow_warp(features, forward_flows[:, t-1, :, :, :]) # feature warping (W step)
-            features = torch.cat((features, lr_curr), dim=1)
-            features = self.forward_res(features) # residual processing (R step)
-        
-            # fuse forward and backward features, then upsample
-            bidirectional_features = torch.cat((features, outputs[t]), dim=1)
-            fused = self.lrelu(self.fusion(bidirectional_features))
-            out = F.pixel_shuffle(self.lrelu(self.up_conv1(fused)), 2)
-            out = F.pixel_shuffle(self.lrelu(self.up_conv2(out)), 2)
-            out = self.hr_conv(out)
-            out += F.interpolate(lr_curr, scale_factor=4.0, mode='bilinear', align_corners=False) # add residuals to upsampled input
-            outputs[t] = out
-            
-        return torch.stack(outputs, dim=1) # turn list of NCHW tensors with length T into a NTCHW tensor
-
